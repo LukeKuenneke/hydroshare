a8167e712e1af0c51e0e524ec0c33e512f0d1bfd
Alva Couch <acouch@cuahsi.org>
2017-04-24 14:42:22 -0400

    Add command code

--- hs_core/management/commands/check_irods_files.py
+++ hs_core/management/commands/check_irods_files.py
@@ -8,6 +8,9 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django_irods.icommands import SessionException
 from django_irods.storage import IrodsStorage
+from django.core.management.base import BaseCommand
+
+from hs_core.models import BaseResource
 
 # This is a clone of the migration code from 1.9.* to 1.10.0, turned into a file checker.
 # It is a bit more careful than the migrations, in the sense that Django is running and
@@ -527,3 +530,57 @@ def __resource_check_irods_directory(self, dir, logger, all_paths,
             raise ValidationError(msg)
 
     return errors, ecount  # empty unless return_errors=True
+
+# -*- coding: utf-8 -*-
+
+"""
+Check synchronization between iRODS and Django
+
+This checks that:
+
+1. every ResourceFile corresponds to an iRODS file
+2. every iRODS file in {short_id}/data/contents corresponds to a ResourceFile
+
+* By default, prints errors on stdout.
+* Optional argument --log instead logs output to system log.
+"""
+
+
+class Command(BaseCommand):
+    help = "Check synchronization between iRODS and Django."
+
+    def add_arguments(self, parser):
+
+        # a list of resource id's, or none to check all resources
+        parser.add_argument('resource_ids', nargs='*', type=str)
+
+        # Named (optional) arguments
+        parser.add_argument(
+            '--log',
+            action='store_true',  # True for presence, False for absence
+            dest='log',           # value is options['log']
+            help='log errors to system log',
+        )
+
+    def handle(self, *args, **options):
+        if len(options['resource_ids']) > 0:  # an array of resource short_id to check.
+            for rid in options['resource_ids']:
+                try:
+                    resource = BaseResource.objects.get(short_id=rid)
+                except BaseResource.DoesNotExist:
+                    msg = "Resource with id {} not found in Django Resources".format(rid)
+                    print(msg)
+
+                print("LOOKING FOR ERRORS FOR RESOURCE {}".format(rid))
+                resource.check_irods_files(stop_on_error=False,
+                                           echo_errors=not options['log'],
+                                           log_errors=options['log'],
+                                           return_errors=False)
+
+        else:  # check all resources
+            print("LOOKING FOR ERRORS FOR ALL RESOURCES")
+            for r in BaseResource.objects.all():
+                r.check_irods_files(stop_on_error=False,
+                                    echo_errors=not options['log'],  # Don't both log and echo
+                                    log_errors=options['log'],
+                                    return_errors=False)
